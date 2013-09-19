fc00863bac8451b79bbf5b344746a47133a1e642
rdowling <rdowling@areteinc.com>
2013-09-19 07:28:55 -0700

    [RPD, LPC] #3 - Added functionality to set modification times in schema handler.

--- rb/schema.rb
+++ rb/schema.rb
@@ -1,7 +1,15 @@
 
 class Schema
   
+  def initialize() 
+    self.up_mod_time("08/20/13 09:00")
+  end
+
   def get_mod_time
-    return "08/20/13 09:00"
+    return @mod_time
+  end
+
+  def up_mod_time(s_time)
+    @mod_time = s_time
   end
 end
